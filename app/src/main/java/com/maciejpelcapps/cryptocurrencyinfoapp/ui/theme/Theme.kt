03ed85938df4bab00b4c6932829512e85c6c525e
Maciej Pelc <78445032+Pelc314@users.noreply.github.com>
2023-01-23 21:02:01 +0100

    added theme and dependencies

--- app/src/main/java/com/maciejpelcapps/cryptocurrencyinfoapp/ui/theme/Theme.kt
+++ app/src/main/java/com/maciejpelcapps/cryptocurrencyinfoapp/ui/theme/Theme.kt
@@ -1,35 +1,49 @@
-package com.maciejpelcapps.cryptocurrencyinfoapp.ui.theme
+package com.maciejpelcapps.cryptocurrencyinfoapp.ui.theme // ktlint-disable filename
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+// private val DarkColorPalette = darkColors(
+//    primary = Purple200,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+// )
+//
+// private val LightColorPalette = lightColors(
+//    primary = Purple500,
+//    primaryVariant = Purple700,
+//    secondary = Teal200
+//
+//    /* Other default colors to override
+//    background = Color.White,
+//    surface = Color.White,
+//    onPrimary = Color.White,
+//    onSecondary = Color.Black,
+//    onBackground = Color.Black,
+//    onSurface = Color.Black,
+//    */
+// )
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = ColorPrimary,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = ColorPrimary,
     background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onBackground = MediumGray,
+    onPrimary = DarkGray
 )
 
 @Composable
 fun CryptocurrencyInfoAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
